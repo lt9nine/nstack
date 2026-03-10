5375af0c82560c3cd433093dafa56d7c0674d471
lt9nine <ben@grunden.me>
2026-03-10 22:26:21 +0100

    fixed leak

--- gamemode/nstack/services/database/credentials.lua
+++ gamemode/nstack/services/database/credentials.lua
@@ -1,5 +1,5 @@
-nstack.services[ "database" ].settings.host = "136.243.175.175"
-nstack.services[ "database" ].settings.user = "u1_7rnnZakNyS"
-nstack.services[ "database" ].settings.pass = "MgxOlkst.z+@dzpUBXlIPOua"
-nstack.services[ "database" ].settings.name = "s1_dev"
-nstack.services[ "database" ].settings.port = 3306
+nstack.services[ "database" ].settings.host = ""
+nstack.services[ "database" ].settings.user = ""
+nstack.services[ "database" ].settings.pass = ""
+nstack.services[ "database" ].settings.name = ""
+nstack.services[ "database" ].settings.port = 0000
