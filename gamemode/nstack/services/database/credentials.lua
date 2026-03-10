d9fb06e5da5f768d703c94b67db121f3330dc9ed
lt9nine <ben@grunden.me>
2026-03-10 22:32:08 +0100

    Added .gitignore for the credentials.lua

--- gamemode/nstack/services/database/credentials.lua
+++
@@ -1,5 +0,0 @@
-nstack.services[ "database" ].settings.host = ""
-nstack.services[ "database" ].settings.user = ""
-nstack.services[ "database" ].settings.pass = ""
-nstack.services[ "database" ].settings.name = ""
-nstack.services[ "database" ].settings.port = 0000
